4f00070595c7e7b57e59f82f94642e6f6aca5f71
G. Avelino <gabrielavelinosilva13@gmail.com>
2025-09-16 20:20:32 -0300

    Add files via upload

---
+++ Atividade02GabrielAvelinoJava.java
@@ -0,0 +1,53 @@
+package atividade02gabrielavelino.java;
+
+import java.util.Scanner;
+
+public class Atividade02GabrielAvelinoJava {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        // 1
+
+        /*System.out.println("Raio do Círculo: ");
+        double raioCirculo = scanner.nextDouble();
+
+        double AreaCirculo = 3.14 * (Math.pow(raioCirculo, 2));
+        double circunCirculo = 2 * 3.14 * raioCirculo;
+
+        System.out.println(AreaCirculo);
+        System.out.println(circunCirculo);
+
+        scanner.close();*/
+
+        // 2
+
+        /*System.out.println("Peso: ");
+        double peso = scanner.nextDouble();
+        
+        System.out.println("Altura: ");
+        double altura = scanner.nextDouble();
+        
+        double IMC = peso / Math.pow(altura, 2);
+        
+        System.out.println(IMC); */
+        
+        // 3
+        
+        /* System.out.print("Cateto 1: ");
+        double cateto1 = scanner.nextDouble();
+        
+        System.out.print("Cateto 2: ");
+        double cateto2 = scanner.nextDouble();
+        
+        double hipotenusa = Math.sqrt(Math.pow(cateto1, 2) + Math.pow(cateto2, 2));
+        
+        System.out.print(hipotenusa); */
+
+        // 4 
+        // a)
+        
+        
+        
+        
+    }
+}
