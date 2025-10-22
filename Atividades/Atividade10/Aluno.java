2267f55b4513ba5682203d8d4b74c4f2e2269c9e
Gabriel Avelino <gabrielavelinosilva13@gmail.com>
2025-10-22 18:58:47 -0300

    Create Aluno.java

---
+++ Atividades/Atividade10/Aluno.java
@@ -0,0 +1,33 @@
+
+package atividade10;
+
+public class Aluno {
+   
+    private int id;
+    private String nome;
+
+    public Aluno() {
+    }
+    
+    public Aluno(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+}
